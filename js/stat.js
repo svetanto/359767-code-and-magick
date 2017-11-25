ece23c6cde14071b7c34beb854a38626d2e0d21d
svetanto <svetanto@gmail.com>
2017-11-25 18:58:41 +0300

    Исправлены вставки исправления конфликта

--- js/stat.js
+++ js/stat.js
@@ -1,27 +1,7 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-<<<<<<< HEAD
-  ctx.fillStyle = 'white';
 
-
-  var startAngle = 15*Math.PI/7;
-  var endAngle = 13*Math.PI/2;
-  var counterClockwise = true;//направление дуги. По умолчанию false
-
-  //ширина и цвет круга
-  ctx.lineWidth = 1;
-  ctx.strokeStyle = 'blue';
-
-  //начало пути, рисуем круг с нашими параметрами и обводим
-  ctx.beginPath();
-  ctx.arc(50, 50, 50, startAngle, endAngle, counterClockwise);
-  ctx.stroke();
-  ctx.closePath();
-  ctx.fill = 'white';
-
-};
-=======
   var shiftFromLeft = 130;
   var shiftFromTop = 10;
   var fullCircle = 2 * Math.PI;
@@ -102,4 +82,3 @@ function getColor(name) {
   + (Math.random() * (maxColorValue - minColorValue) + minColorValue).toFixed(0) + ','
   + (Math.random() * (maxColorValue - minColorValue) + minColorValue).toFixed(0) + ')';
 }
->>>>>>> c0cb8c1a012b9f5e209e2824b50043cf4d5a584c
